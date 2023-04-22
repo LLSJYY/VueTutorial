5b3e263385b0d6e075d5f4a2334750f6a4eeb167
이쥬곰 <leesu@ijyugom-ui-MacBookAir.local>
2023-04-22 17:44:21 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { reactive, ref } from 'vue'
+const newTodo = ref()
+const newTodoHandler = () => {
+  console.log(newTodo.value)
+}
+</script>
+
+<template>
+  <section class="todoapp">
+    <header class="header">
+      <h1>todos</h1>
+      <input class="new-todo" autofocus placeholder="What needs to be done?" @change="" />
+    </header>
+  </section>
+</template>
+
+<style scoped>
+@import 'https://unpkg.com/todomvc-app-css@2.4.1/index.css';
+</style>
